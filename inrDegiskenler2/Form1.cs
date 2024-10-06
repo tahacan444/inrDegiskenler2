0c703c355fa8faa0bc8ba7bf7fd905d436d75f71
tahacan444 <tahacane5@gmail.com>
2024-10-06 13:05:21 +0300

    Proje dosyası ekle.

---
+++ inrDegiskenler2/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace inrDegiskenler2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi,sonuc;
+            //sayi = Convert.ToInt16(textBox1.Text);
+            //label2.Text = sayi.ToString();
+
+
+            sayi = Convert.ToInt16(textBox1.Text);
+            sonuc = (int)Math.Pow(sayi,3);
+            label2.Text=sonuc.ToString();
+        }
+    }
+}
